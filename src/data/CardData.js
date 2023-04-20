38ec11740c56ec47b96ea43387bfa124f93efbff
deorepallavi70 <109799433+deorepallavi70@users.noreply.github.com>
2023-04-20 15:43:01 +0530

    data folder

---
+++ src/data/CardData.js
@@ -0,0 +1,65 @@
+export let cardData = [
+  {
+    id: 1,
+    title: "kurti",
+    discription:
+      "A kurta is a loose collarless shirt or tunic worn in many regions of South Asia, and now also worn around the world. Tracing its roots to Central Asian ...",
+    likes: 9,
+    img: "./../../../img/ShoppingData/kurta1.jpg",
+    price: 1630,
+    qty:1,
+    sizes: ["XS", "S", "M", "L", "XL", "XXL"],
+    rating : 4,
+  },
+
+  {
+    id: 2,
+    title: "Bag",
+    discription:
+      "Bag tunic worn in many regions of South Asia, and now also worn around the world. Tracing its roots to Central Asian ...",
+    likes: 5,
+    img: "./../../../img/ShoppingData/bag.jpeg",
+    price: 1000,
+    qty:1,
+    sizes: ["S", "M", "L"],
+    rating : 4,
+  },
+
+  {
+    id: 3,
+    title: "cap",
+    discription:
+      "  Cap now also worn around the world. Tracing its roots to Central Asian ...",
+    likes: 15,
+    img: "./../../../img/ShoppingData/cap.jpeg",
+    price: 680,
+    qty:1,
+    sizes: ["XS", "S", "M"],
+    rating : 4,
+  },
+  {
+    id: 4,
+    title: "Headphone",
+    discription:
+      "   headphone    worn in many regions of South Asia, and now also worn around the world. Tracing its roots to Central Asian ...",
+    likes:4,
+    img: "./../../../img/ShoppingData/headphones.png",
+    price: 1630,
+    qty:1,
+    sizes: ["XS", "S", "M"],
+    rating : 4,
+  },
+
+  {
+    id: 5,
+    title: "kurti",
+    discription:
+      "A kurta is a loose collarless shirt or tunic worn in many regions of South Asia, and now also worn around the world. Tracing its roots to Central Asian ...",
+    likes: 9,
+    img: "./../../../img/ShoppingData/kurta2.jpg",
+    price: 1000,
+    qty:1,
+    sizes: ["XS", "S", "M", "L", "XL", "XXL"],
+    rating : 4,
+  },
+];
